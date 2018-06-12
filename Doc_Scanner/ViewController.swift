c9259f5a850db8117b44cbbd7fb1e3d3f0eb5dc6
kazupong <kzpng2@gmail.com>
2018-06-12 13:57:26 -0700

    Initial Commit

---
+++ Doc_Scanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Doc_Scanner
+//
+//  Created by Kazuyuki Nakatsu on 6/12/18.
+//  Copyright © 2018 Kazuyuki Nakatsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
